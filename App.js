6f9086c49e02f3e4eafaa8ac2fbde0044f1dc640
Leonardo Santos <leoluisbh@hotmail.com>
2021-03-11 19:20:19 -0300

    IMC Project

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  function handleSubmit() {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if (imc < 18.6) {
+      alert('Você está abaixo do peso !' + imc.toFixed(2));
+    } else if (imc >= 18.6 && imc < 24.9) {
+      alert('Peso ideal ! ' + imc.toFixed(2));
+    } else if (imc >= 24.9 && imc < 34.9) {
+      alert('Levemente acima do peso! ' + imc.toFixed(2));
+    } else {
+      alert('Acima do peso, cuidado! ' + imc.toFixed(2));
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Calcule seu IMC</Text>
+      <TextInput
+        style={styles.input}
+        value={peso}
+        onChangeText={(peso) => setPeso(peso)}
+        placeholder='Peso em (kg)'
+        placeholderTextColor="white"
+        keyboardType='numeric'
+      />
+      <TextInput
+        style={styles.input}
+        value={altura}
+        onChangeText={(altura) => setAltura(altura)}
+        placeholder='Altura em (cm)'
+        placeholderTextColor="white"
+        keyboardType='numeric'
+      />
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={handleSubmit}
+      >
+        <Text style={styles.btnTxt}>Calcular</Text>
+      </TouchableOpacity>
+
     </View>
   );
-}
 
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    textAlign: 'center',
+    marginTop: 25,
+    fontSize: 30
+  },
+  input: {
+    backgroundColor: '#121212',
+    borderRadius: 10,
+    margin: 15,
+    padding: 10,
+    color: '#FFFFFF',
+    fontSize: 23
+
+  },
+  btn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    backgroundColor: '#41Aef4',
+    padding: 10,
+    borderRadius: 10
+  },
+  btnTxt: {
+    color: "#fff",
+    fontSize: 25
+  }
 });
